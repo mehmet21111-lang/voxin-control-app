db7c1a5143df3a86bcda61af4dae1e248f249f7a
mehmet21111-lang <mehmet21111@gmail.com>
2026-06-08 18:55:41 +0300

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,17 +3,90 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  Widget bilgiKarti(
+      IconData icon,
+      String baslik,
+      String deger,
+      Color renk,
+      ) {
+    return Card(
+      elevation: 4,
+      child: ListTile(
+        leading: Icon(icon, color: renk, size: 35),
+        title: Text(baslik),
+        trailing: Text(
+          deger,
+          style: const TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('VoxIN Control'),
+        title: const Text("VoxIN Control"),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
       ),
-      body: const Center(
-        child: Text(
-          'TCL VoxIN Ana Ekran',
-          style: TextStyle(fontSize: 24),
-        ),
+      body: ListView(
+        padding: const EdgeInsets.all(16),
+        children: [
+
+          bilgiKarti(
+            Icons.thermostat,
+            "İç Sıcaklık",
+            "24°C",
+            Colors.red,
+          ),
+
+          bilgiKarti(
+            Icons.cloud,
+            "Dış Sıcaklık",
+            "31°C",
+            Colors.orange,
+          ),
+
+          bilgiKarti(
+            Icons.water_drop,
+            "Nem",
+            "%52",
+            Colors.blue,
+          ),
+
+          bilgiKarti(
+            Icons.flash_on,
+            "Anlık Güç",
+            "420 W",
+            Colors.green,
+          ),
+
+          bilgiKarti(
+            Icons.speed,
+            "Kompresör",
+            "58 Hz",
+            Colors.purple,
+          ),
+
+          bilgiKarti(
+            Icons.bolt,
+            "Bugünkü Tüketim",
+            "2.8 kWh",
+            Colors.teal,
+          ),
+
+          const SizedBox(height: 20),
+
+          ElevatedButton.icon(
+            onPressed: () {},
+            icon: const Icon(Icons.power_settings_new),
+            label: const Text("Klimayı Aç / Kapat"),
+          ),
+        ],
       ),
     );
   }
